d0cf51bb70c7ed78bef82eaa2e7f921a8f0b11d3
matthewjohnson <nosnhojdm@gmail.com>
2018-01-29 00:27:17 -0800

    Simon the Robot

---
+++ Project -v1.0.2 - 4.2.0/Assets/InteractableRobot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableRobot : MonoBehaviour {
+
+	[Tooltip("Priority is used to shwo when the line is played.")]
+	public List<DialogLine> myLines;
+	public AudioSource mySrc;
+
+	DialogLine currentLine;
+	void Start()
+	{
+
+		currentLine = myLines.Find (item => item.Priority == LevelData.getHighestLevel ());
+		if (currentLine!= null) {
+
+
+		} else {
+		
+			this.gameObject.SetActive (false);
+		}
+
+	}
+
+
+	public void interact()
+	{
+		mySrc.PlayOneShot (currentLine.MainLine.myClip);
+		currentLine = null;
+		turnOffFlashing ();
+	}
+
+
+	public void turnOffFlashing()
+	{
+		
+	}
+
+}
