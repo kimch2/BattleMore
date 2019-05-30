8f08a6946f6f57b2388a28efe7877b72583b88c6
Matthew <nosnhojdm@gmail.com>
2019-05-29 21:27:24 -0700

    3rd cleanup commit

---
+++ Project -v1.0.2 - 4.2.0/Assets/ArtResources/Effects/BasicBeamShot/Script/GetColor_Particle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetColor_Particle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ParticleSystem ps = this.gameObject.GetComponent<ParticleSystem>();
+
+        ParticleSystem.MainModule ma = ps.main;
+
+		ma.startColor = this.transform.root.gameObject.GetComponent<BeamParam>().BeamColor;
+		ma.startSize = ma.startSize.constant * this.transform.root.gameObject.GetComponent<BeamParam>().Scale;
+	}
+	
+}
---
+++ Project -v1.0.2 - 4.2.0/Assets/Prefabs/BunnyAssaultMode/Squirrel/Scripts/Squirrel.cs
@@ -0,0 +1,166 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Squirrel : MonoBehaviour {
+    private IEnumerator coroutine;
+    Animator squirrel;
+
+	// Use this for initialization
+	void Start () {
+        squirrel = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("up"))
+        {
+            squirrel.SetBool("run", true);
+            squirrel.SetBool("idle", false);
+            squirrel.SetBool("runleft", false);
+            squirrel.SetBool("runright", false);
+            squirrel.SetBool("walk", false);
+            squirrel.SetBool("left", false);
+            squirrel.SetBool("right", false);
+            squirrel.SetBool("stand", false);
+            squirrel.SetBool("eat", false);
+        }
+        if ((Input.GetKey("down")) || (Input.GetKey(KeyCode.S)))
+        {
+            squirrel.SetBool("idle", true);
+            squirrel.SetBool("run", false);
+            squirrel.SetBool("runleft", false);
+            squirrel.SetBool("runright", false);
+            squirrel.SetBool("walk", false);
+            squirrel.SetBool("left", false);
+            squirrel.SetBool("right", false);
+            squirrel.SetBool("stand", false);
+            squirrel.SetBool("eat", false);
+        }
+        if (Input.GetKey("left"))
+        {
+            squirrel.SetBool("runleft", true);
+            squirrel.SetBool("runright", false);
+            squirrel.SetBool("run", false);
+            squirrel.SetBool("idle", false);
+            squirrel.SetBool("walk", false);
+            squirrel.SetBool("left", false);
+            squirrel.SetBool("right", false);
+            squirrel.SetBool("stand", false);
+            squirrel.SetBool("eat", false);
+            squirrel.SetBool("stand", false);
+        }
+        if (Input.GetKey("right"))
+        {
+            squirrel.SetBool("runright", true);
+            squirrel.SetBool("runleft", false);
+            squirrel.SetBool("run", false);
+            squirrel.SetBool("idle", false);
+            squirrel.SetBool("walk", false);
+            squirrel.SetBool("left", false);
+            squirrel.SetBool("right", false);
+            squirrel.SetBool("stand", false);
+            squirrel.SetBool("eat", false);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            squirrel.SetBool("walk", true);
+            squirrel.SetBool("left", false);
+            squirrel.SetBool("right", false);
+            squirrel.SetBool("run", false);
+            squirrel.SetBool("runleft", false);
+            squirrel.SetBool("runright", false);
+            squirrel.SetBool("idle", false);
+            squirrel.SetBool("stand", false);
+            squirrel.SetBool("eat", false);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            squirrel.SetBool("left", true);
+            squirrel.SetBool("walk", false);
+            squirrel.SetBool("right", false);
+            squirrel.SetBool("run", false);
+            squirrel.SetBool("runleft", false);
+            squirrel.SetBool("runright", false);
+            squirrel.SetBool("idle", false);
+            squirrel.SetBool("stand", false);
+            squirrel.SetBool("eat", false);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            squirrel.SetBool("right", true);
+            squirrel.SetBool("left", false);
+            squirrel.SetBool("walk", false);
+            squirrel.SetBool("run", false);
+            squirrel.SetBool("runleft", false);
+            squirrel.SetBool("runright", false);
+            squirrel.SetBool("idle", false);
+            squirrel.SetBool("stand", false);
+            squirrel.SetBool("eat", false);
+        }
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            squirrel.SetBool("up", true);
+            squirrel.SetBool("walk", false);
+            squirrel.SetBool("right", false);
+            squirrel.SetBool("left", false);
+            squirrel.SetBool("run", false);
+            squirrel.SetBool("runleft", false);
+            squirrel.SetBool("runright", false);
+            squirrel.SetBool("idle", false);
+            StartCoroutine("stand");
+            stand();
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            squirrel.SetBool("down", true);
+            squirrel.SetBool("up", false);
+            squirrel.SetBool("walk", false);
+            squirrel.SetBool("right", false);
+            squirrel.SetBool("run", false);
+            squirrel.SetBool("runleft", false);
+            squirrel.SetBool("runright", false);
+            squirrel.SetBool("idle", false);
+            squirrel.SetBool("stand", false);
+            StartCoroutine("stand");
+            StartCoroutine("idle");
+            idle();
+        }
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            squirrel.SetBool("eat", true);
+            squirrel.SetBool("down", false);
+            squirrel.SetBool("up", false);
+            squirrel.SetBool("walk", false);
+            squirrel.SetBool("right", false);
+            squirrel.SetBool("left", false);
+            squirrel.SetBool("run", false);
+            squirrel.SetBool("runleft", false);
+            squirrel.SetBool("runright", false);
+            squirrel.SetBool("idle", false);
+            squirrel.SetBool("stand", false);
+        }
+        if (Input.GetKey(KeyCode.Alpha0))
+        {
+            squirrel.SetBool("die", true);
+            squirrel.SetBool("run", false);
+            squirrel.SetBool("runleft", false);
+            squirrel.SetBool("runright", false);
+        }
+    }
+    IEnumerator stand()
+    {
+       // print(Time.time);
+        yield return new WaitForSeconds(0.5f);
+       // print(Time.time);
+        squirrel.SetBool("stand", true);
+        squirrel.SetBool("up", false);
+    }
+    IEnumerator idle()
+    {
+       // print(Time.time);
+        yield return new WaitForSeconds(0.5f);
+      //  print(Time.time);
+        squirrel.SetBool("idle", true);
+        squirrel.SetBool("down", false);
+    }
+}
